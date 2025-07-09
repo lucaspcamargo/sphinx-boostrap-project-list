efee96dc0924aefc10a8bccbd691ef41bece9795
Lucas Camargo <lucas@camargo.eng.br>
2025-07-09 14:52:03 +0200

    initial project skeleton

---
+++ src/sphinx_bootstrap_project_list/__init__.py
@@ -0,0 +1,11 @@
+
+__title__= 'sphinx-bootstrap-project-list'
+__license__ = 'GPLv3'
+__version__ = '1.0'
+__author__ = 'Lucas Pires Camargo'
+__url__ = 'https://camargo.eng.br'
+__description__ = 'Sphinx extension for rendering nice-looking project lists using Bootstrap 5'
+__keywords__ = 'documentation, sphinx, extension, bootstrap, html'
+
+def setup(app):
+    pass
